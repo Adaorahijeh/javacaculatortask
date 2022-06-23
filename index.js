36464309d46667fa96d36d4547300de677cf2cf4
adaorah ijeh <adaorahijeh8@gmail.com>
2022-06-23 23:06:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function calculator(){
+      let operator = prompt("Enter Operator(e.g + / - or *): ");
+      let firstNumber = parseFloat(prompt("Enter First Number: "));
+      let secondNumber = parseFloat(prompt("Enter Second Number: "));
+           
+      let result = null;
+     if (isNaN(firstNumber) || isNaN(secondNumber)){
+           alert("Invalid Input")
+           calculator();
+        }
+        else{
+           if (operator == '+'){
+                 result = firstNumber + secondNumber;
+           }
+           else if(operator == '/'){
+                 result = firstNumber / secondNumber;
+           }
+           else if(operator == '-'){
+                 result = firstNumber - secondNumber;
+           }
+           else if(operator == '*'){
+            result = firstNumber * secondNumber;
+           }
+            alert('Result is ' + result);
+        }
+        
+     
+     }
+     
+     calculator();
